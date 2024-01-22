0b6fb8f449f1a2b74fa5a51ea7cd39f8b228abae
frako23 <frako23@gmail.com>
2024-01-21 20:06:14 -0400

    first commit

---
+++ app/interface.ts
@@ -0,0 +1,8 @@
+export interface SimplifiedProduct {
+  _id: string;
+  imageUrl: string;
+  price: number;
+  slug: string;
+  categoryName: string;
+  name: string;
+}
---
+++ sanity-project/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'kmjsjl34',
+    dataset: 'production'
+  }
+})
